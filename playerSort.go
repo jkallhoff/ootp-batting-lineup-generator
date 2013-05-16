36027ac6b58ce4ebd4e7eecdc517e11950dd3089
Jesse Kallhoff <jkallhof@nerdery.com>
2013-05-16 13:27:37 -0500

    Update to add sorting by fields. Break out the html parsing into separate file

---
+++ playerSort.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"sort"
+)
+
+type playerLessFunc func(player1, player2 *Player) bool
+
+type playerSorter struct {
+	players     []*Player
+	sortMethods []playerLessFunc
+}
+
+func (sorter *playerSorter) Sort() {
+	sort.Sort(sorter)
+}
+
+func OrderedBy(players []*Player, sortMethods ...playerLessFunc) *playerSorter {
+	return &playerSorter{
+		players:     players,
+		sortMethods: sortMethods,
+	}
+}
+
+func (ps *playerSorter) Len() int {
+	return len(ps.players)
+}
+
+func (ps *playerSorter) Swap(i, j int) {
+	ps.players[i], ps.players[j] = ps.players[j], ps.players[i]
+}
+
+func (ps *playerSorter) Less(i, j int) bool {
+	p, q := ps.players[i], ps.players[j]
+
+	var k int
+	for k = 0; k < len(ps.sortMethods)-1; k++ {
+		sortMethod := ps.sortMethods[k]
+		switch {
+		case sortMethod(p, q):
+			return true
+		case sortMethod(q, p):
+			return false
+		}
+	}
+	return ps.sortMethods[k](p, q)
+}
+
+//Column Sorts
+func sortByIncreasingAge(player1, player2 *Player) bool {
+	return player1.Age < player2.Age
+}
+
+func sortByDecreasingAge(player1, player2 *Player) bool {
+	return player1.Age > player2.Age
+}
+
+func sortByDecreasingAtBats(player1, player2 *Player) bool {
+	return player1.AtBats > player2.AtBats
+}
